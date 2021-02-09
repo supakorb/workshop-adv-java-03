75addd49bde6189aa116da73ef51719bd789b447
supakorn.s <supakorn.s@kbtg.tech>
2021-02-09 16:03:40 +0700

    refactor code

--- src/main/java/badcode/RegisterBusiness.java
+++ src/main/java/badcode/RegisterBusiness.java
@@ -1,47 +1,37 @@
 package badcode;
 
-import java.util.Arrays;
-
 public class RegisterBusiness {
 
 	public Integer register(SpeakerRepository repository, Speaker speaker) {
 		Integer speakerId;
 		String[] domains = { "gmail.com", "live.com" };
 		
-		if (speaker.getFirstName() == null || speaker.getFirstName().trim().equals("")) {
-			throw new ArgumentNullException("First name is required.");
-		}
-		if (speaker.getLastName() == null || speaker.getLastName().trim().equals("")) {
-			throw new ArgumentNullException("Last name is required.");
-		}
-		if (speaker.getEmail() == null || speaker.getEmail().trim().equals("")) {
-			throw new ArgumentNullException("Email is required.");
-		}
-		int indexDomain = speaker.getEmail().indexOf("@");
-		boolean isValidDomain = indexDomain >= 0 && indexDomain != speaker.getEmail().length() - 1;
-		String emailDomain =  isValidDomain ? speaker.getEmail().split("@")[1] : "";
-		if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 0) {
-			throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
-		}
+		speaker.checkEmptyFirstName();
+		speaker.checkEmptyLastName();
+		speaker.checkValidEmail(domains);
 		
 		int exp = speaker.getExp();
 		// TODO :: create test case to cover this condition
-		if (exp <= 1) {
-			speaker.setRegistrationFee(500);
-		} else if (exp >= 2 && exp <= 3) {
-			speaker.setRegistrationFee(250);
-		} else if (exp >= 4 && exp <= 5) {
-			speaker.setRegistrationFee(100);
-		} else if (exp >= 6 && exp <= 9) {
-			speaker.setRegistrationFee(50);
-		} else {
-			speaker.setRegistrationFee(0);
-		}
+		speaker.setRegistrationFee(getFee(exp));
 		try {
 			speakerId = repository.saveSpeaker(speaker);
 		} catch (Exception exception) {
 			throw new SaveSpeakerException("Can't save a speaker.");
 		}
 		return speakerId;
+	}
+	
+	private int getFee(int exp) {
+		int fee = 0;
+		if (exp <= 1) {
+			fee = 500;
+		} else if (exp <= 3) {
+			fee = 250;
+		} else if (exp <= 5) {
+			fee = 100;
+		} else if (exp <= 9) {
+			fee = 50;
+		}
+		return fee;
 	}
 }
